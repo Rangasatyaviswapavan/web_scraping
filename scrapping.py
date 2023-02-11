3b4211b441e47d366239cd0c14a88eeb486fba75
Rangasatyaviswapavan <53988158+Rangasatyaviswapavan@users.noreply.github.com>
2023-02-11 17:15:50 -0500

    Update scrapping.py

--- scrapping.py
+++ scrapping.py
@@ -1,11 +1,16 @@
 import cloudscraper
 import csv
+import boto3
 import pandas as pd
 scraper = cloudscraper.create_scraper()
 page = scraper.get('https://finviz.com/screener.ashx?v=410&s=ta_topgainers').text
 res = pd.read_html(page)
 lst = res[8][0][0].split()
+s3 = boto3.client('s3')
+bucket_name = 'scraperrresults'
+file_name = 'results.csv'
 fn = "GFG.csv"
 with open(fn,'w') as f:
   csvw =csv.writer(f)
   csvw.writerow(lst)
+s3.upload_file(file_name, bucket_name, file_name)
